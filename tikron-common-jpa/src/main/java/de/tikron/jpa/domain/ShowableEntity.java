9e2900dd04787693fc67eaf6ff6b3a77c4baa9a0
Titus Kruse <contact@tikron.de>
2015-04-29 13:21:34 +0200

    Kommentar erweitert.

--- tikron-common-jpa/src/main/java/de/tikron/jpa/domain/ShowableEntity.java
+++ tikron-common-jpa/src/main/java/de/tikron/jpa/domain/ShowableEntity.java
@@ -3,7 +3,9 @@ package de.tikron.jpa.domain;
 import java.io.Serializable;
 
 /**
- * Declares an enity that could be shown to a human person.
+ * Declares an enity that could be shown to a human person. Entity classes implementing this interface shall only select
+ * or combine appropriate properties to return and don't do any formatting or translation. Otherwise the MVC pattern
+ * will be violeted. For example a cars display name is composed by manufactorer and model name.
  * 
  * @param <ID> The type of primary key.
  *
