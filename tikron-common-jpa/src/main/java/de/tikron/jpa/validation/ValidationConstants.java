6fb85e10d6f8343b6ce75ba17c3516c3d7b62092
Titus Kruse <contact@tikron.de>
2016-01-26 17:24:45 +0100

    E-Mail-Validierung vereinfacht.

--- tikron-common-jpa/src/main/java/de/tikron/jpa/validation/ValidationConstants.java
+++ tikron-common-jpa/src/main/java/de/tikron/jpa/validation/ValidationConstants.java
@@ -11,7 +11,10 @@ package de.tikron.jpa.validation;
  */
 public interface ValidationConstants {
 
-	public static final String EMAIL_MASK = "[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?";
+// Replaced by simple pattern because of endless loop in JSR303 bean validation in hibernate with this complex pattern.
+//	public static final String EMAIL_MASK = "[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?";
+	// @see http://www.regular-expressions.info/email.html
+	public static final String EMAIL_MASK = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
 	
 	// @see http://stackoverflow.com/questions/163360/regular-expression-to-match-urls-in-java
 	public static final String URL_MASK = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
