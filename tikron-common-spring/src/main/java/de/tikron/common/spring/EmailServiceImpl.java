2b66fbf82d96b2efc227940a16c3470f3a8e551b
Titus Kruse <contact@tikron.de>
2017-07-24 06:50:22 +0200

    Auf Java EE 7 umgestellt.

--- tikron-common-spring/src/main/java/de/tikron/common/spring/EmailServiceImpl.java
+++ tikron-common-spring/src/main/java/de/tikron/common/spring/EmailServiceImpl.java
@@ -3,17 +3,11 @@
  */
 package de.tikron.common.spring;
 
-import java.io.UnsupportedEncodingException;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
 /**
  * Stellt Funktionen zum Versenden von E-Mails bereit (Standardimplementierung).
@@ -30,8 +24,6 @@ public class EmailServiceImpl implements EmailService {
 	private String sender;
 
 	private String recipient;
-
-	private String encoding;
 
 	/*
 	 * (non-Javadoc)
@@ -70,39 +62,32 @@ public class EmailServiceImpl implements EmailService {
 	public boolean sendEmail(String sender, String senderName, String recipient, String subject, String content) {
 		try {
 			if (enabled) {
-				MimeMessage message = mailSender.createMimeMessage();
-				MimeMessageHelper helper = new MimeMessageHelper(message, false, encoding);
+				SimpleMailMessage message = new SimpleMailMessage();
 				// Set sender address and name
 				if (senderName != null)
-					helper.setFrom(sender, senderName);
+					message.setFrom(senderName);
 				else if (sender != null)
-					helper.setFrom(new InternetAddress(sender));
+					message.setFrom(sender);
 				else
-					helper.setFrom(new InternetAddress(this.sender));
+					message.setFrom(this.sender);
 				// Set recipient
 				if (recipient != null)
-					helper.setTo(new InternetAddress(recipient));
+					message.setTo(recipient);
 				else
-					helper.setTo(new InternetAddress(this.recipient));
+					message.setTo(this.recipient);
 				// Set subject
 				if (subject != null)
-					helper.setSubject(subject);
+					message.setSubject(subject);
 				else
 					throw new IllegalArgumentException("Missing parameter subject");
 				// Set message text
 				if (content != null)
-					helper.setText(content);
+					message.setText(content);
 				else
 					throw new IllegalArgumentException("Missing parameter content");
 				mailSender.send(message);
 			}
-		} catch (MessagingException e) {
-			e.printStackTrace();
-			return false;
 		} catch (MailException e) {
-			e.printStackTrace();
-			return false;
-		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 			return false;
 		}
@@ -134,11 +119,6 @@ public class EmailServiceImpl implements EmailService {
 	@Required
 	public void setRecipient(String recipient) {
 		this.recipient = recipient;
-	}
-
-	@Required
-	public void setEncoding(String encoding) {
-		this.encoding = encoding;
 	}
 
 }
