c5aaff7fbd9878281bd7e58e87648d5dc01a48e4
Titus Kruse <contact@tikron.de>
2017-08-01 16:21:29 +0200

    Converter für java.time.Duration impl.

---
+++ tikron-common-faces/src/main/java/de/tikron/faces/converter/DurationConverter.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2017 by Titus Kruse.
+ */
+package de.tikron.faces.converter;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * Faces Converter converting between Duration and formatted string. The external representation of Duration will be a time like hh:mm:ss instead of PHhhMmmSss.
+ * 
+ * @date 01.08.2017
+ * 
+ * @author Titus Kruse
+ */
+@FacesConverter("durationConverter")
+public class DurationConverter extends AbstractDateTimeConverter {
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object modelValue) {
+		if (modelValue == null) {
+			return "";
+		}
+
+		if (modelValue instanceof Duration) {
+			LocalTime time = LocalTime.ofNanoOfDay(((Duration) modelValue).toNanos());
+			return getTimeFormatter(context, component).format(time);
+		} else {
+			throw new ConverterException(new FacesMessage(modelValue + " is not a valid Duration"));
+		}
+	}
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String submittedValue) {
+		if (submittedValue == null || submittedValue.isEmpty()) {
+			return null;
+		}
+
+		try {
+			LocalTime time = LocalTime.parse(submittedValue, getTimeFormatter(context, component));
+			return Duration.ofNanos(time.toNanoOfDay());
+		} catch (DateTimeParseException e) {
+			throw new ConverterException(new FacesMessage(submittedValue + " is not a valid local date time"), e);
+		}
+	}
+}
