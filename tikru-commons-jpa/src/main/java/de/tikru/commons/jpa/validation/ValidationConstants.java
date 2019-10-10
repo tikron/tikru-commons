b33fec995aaac0d851572e1a2580cd39fd3f0e5a
Titus Kruse <mail@tituskruse.de>
2019-10-10 20:01:32 +0200

    Constants interfaces in class geändert.

--- tikru-commons-jpa/src/main/java/de/tikru/commons/jpa/validation/ValidationConstants.java
+++ tikru-commons-jpa/src/main/java/de/tikru/commons/jpa/validation/ValidationConstants.java
@@ -9,7 +9,9 @@ package de.tikru.commons.jpa.validation;
  * @date 13.05.2015
  * @author Titus Kruse
  */
-public interface ValidationConstants {
+public final class ValidationConstants {
+
+	private ValidationConstants() {};
 
 // Replaced by simple pattern because of endless loop in JSR303 bean validation in hibernate with this complex pattern.
 //	public static final String EMAIL_MASK = "[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[A-Za-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?";
