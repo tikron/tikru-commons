47d6f4eea946e1ed53bebcf5a4ca99862344ed83
Titus Kruse <mail@tituskruse.de>
2020-12-20 20:07:38 +0100

    Extending slf4j INFO logging with fine, finer, finest

---
+++ tikru-commons-util/src/main/java/de/tikru/commons/util/VerbosableLogger.java
@@ -0,0 +1,37 @@
+/**
+	* Copyright (c) 2020 by Titus Kruse.
+	*/
+
+package de.tikru.commons.util;
+
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+
+/**
+ * Extends the {@link org.slf4j.Logger} with another logging level "fine". This level is finer then info but not debug.
+ * Can be enhanced with finer and finest later on.
+ *
+ * @date Dec 20, 2020
+ * @author Titus Kruse
+ */
+
+public interface VerbosableLogger extends Logger {
+
+	public void fine(String msg);
+
+	public void fine(String format, Object arg1, Object arg2);
+
+	public void fine(String format, Object... arg);
+
+	public void fine(String msg, Throwable t);
+
+	public void fine(Marker marker, String msg);
+
+	public void fine(Marker marker, String format, Object arg1, Object arg2);
+
+	public void fine(Marker marker, String format, Object... arg);
+
+	public void fine(Marker marker, String msg, Throwable t);
+
+	public boolean isFineEnabled();
+}
