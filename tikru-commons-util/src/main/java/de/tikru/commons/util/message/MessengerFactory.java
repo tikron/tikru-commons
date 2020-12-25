74ca2076b70249e5127d69491a6291ebe461b620
Titus Kruse <mail@tituskruse.de>
2020-12-25 16:37:47 +0100

    Merge commons-util 3.4 into master

---
+++ tikru-commons-util/src/main/java/de/tikru/commons/util/message/MessengerFactory.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2015 by Titus Kruse.
+ */
+package de.tikru.commons.util.message;
+
+import java.nio.file.Path;
+
+import de.tikru.commons.util.message.config.MailMessengerConfiguration;
+import de.tikru.commons.util.message.config.MessengerConfiguration;
+
+/**
+ * Constructs a {@link Messenger} depending on type of message to send.
+ *
+ * @date 24.06.2015
+ * @author Titus Kruse
+ */
+public class MessengerFactory {
+	
+	public static Messenger create(MessengerConfiguration config, Path workDirectory) {
+		if (config instanceof MailMessengerConfiguration) {
+			return new MailMessenger((MailMessengerConfiguration) config, workDirectory);
+		} else if (config == null) {
+			return new DefaultMessenger();
+		} else {
+			throw new IllegalArgumentException("Unsupported messenger type.");
+		}
+	}
+}
